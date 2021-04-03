2b4905f6dd15794ad423ce745f5e935d3d94d0fd
kobi wolf <kobiwolf95@gmail.com>
2021-04-04 02:16:18 +0300

    add valuesCols checking solution

--- src/components/Board.js
+++ src/components/Board.js
@@ -16,7 +16,7 @@ function Board({ setValues, values }) {
     }
   }, []);
   const onButtonClick = () => {
-    let arr = Object.values(values);
+    // let arr = Object.values(values);
 
     // if (arr.length < rowsCols.length)
     //   alert('you must fill up all the board in order to check result');
@@ -25,65 +25,162 @@ function Board({ setValues, values }) {
 
     console.log(checkSolution(values));
   };
-  const putValuesToSquares = (inputs) => {
-    //helprs funcs
+  const putValuesToRowsCols = (values) => {
+    //helpers funcs
     function between(x, min, max, y, min2 = min, max2 = max) {
       return x >= min && x <= max && y >= min2 && y <= max2;
     }
-    function fillInputsSquaresObj(cubeNum, value, obj = inputsSquares) {
+    function fillValuesObj(cubeNum, value, obj = valuesSquares) {
       obj[cubeNum]
         ? obj[cubeNum].push(value)
         : (obj[cubeNum] = Array(1).fill(value));
     }
-    const arrOfSets = Array(9).fill(new Set());
-    const inputsSquares = {};
-    for (let row = 0; row < inputs[0].length; row++) {
-      for (let col = 0; col < inputs[0].length; col++) {
-        const value = inputs[row][col];
-        arrOfSets[row].add(value);
+    const arrOfSets = [];
+    const valuesSquares = {};
+    const valuesCols = {};
+    for (let row = 0; row < 9; row++) {
+      arrOfSets.push(new Set(values[row]));
+      for (let col = 0; col < 9; col++) {
+        const value = values[row][col];
+        fillValuesObj(row, values[col][row], valuesCols);
         switch (true) {
           case between(row, 0, 2, col):
-            fillInputsSquaresObj('0', value);
+            fillValuesObj('0', value);
             break;
           case between(row, 0, 2, col, 3, 5):
-            fillInputsSquaresObj('1', value);
+            fillValuesObj('1', value);
             break;
           case between(row, 0, 2, col, 6, 8):
-            fillInputsSquaresObj('2', value);
+            fillValuesObj('2', value);
             break;
           case between(row, 3, 5, col, 0, 2):
-            fillInputsSquaresObj('3', value);
+            fillValuesObj('3', value);
             break;
           case between(row, 3, 5, col, 3, 5):
-            fillInputsSquaresObj('4', value);
+            fillValuesObj('4', value);
             break;
           case between(row, 3, 5, col, 6, 8):
-            fillInputsSquaresObj('5', value);
+            fillValuesObj('5', value);
             break;
           case between(row, 6, 8, col, 0, 2):
-            fillInputsSquaresObj('6', value);
+            fillValuesObj('6', value);
             break;
           case between(row, 6, 8, col, 3, 5):
-            fillInputsSquaresObj('7', value);
+            fillValuesObj('7', value);
             break;
           case between(row, 6, 8, col, 6, 8):
-            fillInputsSquaresObj('9', value);
+            fillValuesObj('8', value);
             break;
           default:
             break;
         }
       }
     }
-    return { inputsSquares, arrOfSets };
+    return { valuesSquares, arrOfSets, valuesCols };
   };
-  const checkSolution = (inputs) => {
-    console.log(inputs);
-    const { inputsSquares, arrOfSets } = putValuesToSquares(inputs);
-    console.log(inputsSquares);
+  const toFillBoard = () => {
+    const a = [
+      5,
+      1,
+      7,
+      6,
+      9,
+      8,
+      2,
+      3,
+      4,
+      2,
+      8,
+      9,
+      1,
+      3,
+      4,
+      7,
+      5,
+      6,
+      3,
+      4,
+      6,
+      2,
+      7,
+      5,
+      8,
+      9,
+      1,
+      6,
+      7,
+      2,
+      8,
+      4,
+      9,
+      3,
+      1,
+      5,
+      1,
+      3,
+      8,
+      5,
+      2,
+      6,
+      9,
+      4,
+      7,
+      9,
+      5,
+      4,
+      7,
+      1,
+      3,
+      6,
+      8,
+      2,
+      4,
+      9,
+      5,
+      3,
+      6,
+      2,
+      1,
+      7,
+      8,
+      7,
+      2,
+      3,
+      4,
+      8,
+      1,
+      5,
+      6,
+      9,
+      8,
+      6,
+      1,
+      9,
+      5,
+      7,
+      4,
+      2,
+      3,
+    ];
+    const clone = JSON.parse(JSON.stringify(values));
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        clone[`${i}`][j] = a[i * 9 + j];
+      }
+    }
+    setValues(clone);
+  };
+  const checkSolution = (valuesRows) => {
+    const { valuesSquares, arrOfSets, valuesCols } = putValuesToRowsCols(
+      valuesRows
+    );
     for (let i = 0; i < arrOfSets.length; i++) {
-      let set = arrOfSets[i];
-      console.log(set);
-      if (set.size !== 9 || inputs[i].reduce((a, b) => a + b, 0) !== 45)
+      if (
+        arrOfSets[i].size !== 9 ||
+        valuesRows[i].reduce((a, b) => a + b, 0) !== 45 ||
+        valuesSquares[i].reduce((a, b) => a + b, 0) !== 45 ||
+        valuesCols[i].reduce((a, b) => a + b, 0) !== 45
+      )
         return false;
     }
     return true;
@@ -105,6 +202,7 @@ function Board({ setValues, values }) {
           })}
         </div>
         <Button text="check" func={onButtonClick} />
+        <Button text="fill" func={toFillBoard} />
       </>
     );
   };
